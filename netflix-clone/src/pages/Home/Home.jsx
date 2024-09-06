2737848047b30cba854657f174daef6020a47b74
KedirABD <kedir_abdurohman@yahoo.com>
2024-09-06 09:49:16 +0300

    Banner

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import Banner from '../../components/banner/Banner'
 
 function Home() {
   return (
     <>
       <Header />
+      <Banner />
       <Footer />
     </>
   )
